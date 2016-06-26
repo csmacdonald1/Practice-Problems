6914e793bfa7e38f3a1291dc00c5d3f29349de9c
Chris MacDonald <csmacdonald1@users.noreply.github.com>
2016-06-26 14:48:29 -0400

    add 1.4 and 1.5

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
-
+import java.util.Stack;
+import java.util.NoSuchElementException;
+import java.lang.Math;
 
 class Main {
 
@@ -66,6 +68,75 @@ class Main {
 		return phrase;
 	}
 	
+	//1.4 -- check to see if a string can be rearranged to form a palindrome
+	//		 TODO -- create my own Stack class 	
+	public static boolean palindromePermutation (String str) {
+		
+		//make string lower case and sort it to compare adjacent letters
+		str.toLowerCase();
+		char[] charStr = str.toCharArray();
+		Arrays.sort(charStr);
+
+		Stack stack = new Stack();
+		for (int i = 0 ; i < charStr.length ; i++) {
+			if ( stack.isEmpty() ) {
+				continue;
+			} else if ( stack.peek().toString().charAt(0) != charStr[i] && charStr[i] != ' ' ) {
+				stack.push(charStr[i]);
+			} else {
+				stack.pop();
+			}
+		}
+		
+		//if there are 0 or 1 letters left on the stack, all of the string's letters form a palindrome
+		if ( stack.size() < 2 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//1.5 --
+
+	public static boolean oneAway(String str1, String str2) {
+		int lengthDiff =  Math.abs(str1.length() - str2.length());	
+		if ( lengthDiff > 1 ) {
+			return false;
+		} else if ( lengthDiff == 1 ) {
+			String longWord;
+			String shortWord;
+			if ( str1.length() > str2.length() ) {
+				longWord = str1;
+				shortWord = str2;
+			} else {
+				longWord = str2;
+				shortWord = str1;
+			}
+			int skips = 0;
+			for ( int j = 0 ; j < shortWord.length() ; j++ ) {
+				if ( shortWord.charAt(j) != longWord.charAt(j + skips) ) {
+					skips += 1;
+					if ( skips > 1 || shortWord.charAt(j) != longWord.charAt(j + skips) ) {
+						return false;
+					}
+				}
+			}
+			return true;
+		} else {
+			boolean oneDiff = false;
+			for ( int i = 0 ; i < str1.length() ; i++ ) {
+				if ( str1.charAt(i) != str2.charAt(i) ) {
+					if ( oneDiff ) { //if there is already one difference, can't have 2 differences
+						return false;
+					} else {
+						oneDiff = true; //found the first difference
+					}
+				}
+			}
+			return oneDiff; //NOTE: can't just return true because might not have found any diffs
+		}
+	}	
+
 	public static void main (String[] args ) {
 		
 		//check 1.1
@@ -85,5 +156,13 @@ class Main {
 		char [] url = urlify(str.toCharArray() , length);
 	
 		System.out.println(url);
+
+		
+		//check 1.4
+		System.out.println("Is \"Tact Coa\" a palindrome permutation?\t" + palindromePermutation("Tact Coa"));
+
+		//check 1.5
+		System.out.println("Is \"tac\" one away from \"taco\"?\t" + oneAway("tac", "taco"));
 	}
+
 }
